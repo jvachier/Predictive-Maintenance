653e1082d981afdb716bf4e660dd6e74e465166a
Jeremy Vachier <89128100+jvachier@users.noreply.github.com>
2024-01-27 10:37:33 +0100

    Improve loading.py

--- src/modules/loading.py
+++ src/modules/loading.py
@@ -1,38 +1,40 @@
-import pandas as pd
-import pickle
-
-from dataclasses import dataclass
 from typing import Tuple
+from dataclasses import dataclass
+import pickle
+import pandas as pd
 
 
 @dataclass(slots=True)
 class Loading_files:
+    """
+    Class to load and save required data into a pickle format
+    """
+
     def load_save_df(
         self,
     ) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+        """
+        Saving csv files into pickle ones.
+
+        Return: 5 dataframes for each required files
+        """
         df_telemetry = pd.read_csv("../src/data/PdM_telemetry.csv")
         df_machines = pd.read_csv("../src/data/PdM_machines.csv")
         df_failures = pd.read_csv("../src/data/PdM_failures.csv")
         df_errors = pd.read_csv("../src/data/PdM_errors.csv")
         df_maintenance = pd.read_csv("../src/data/PdM_maint.csv")
 
-        dbfile_telemetry = open("./pickle_files/loading/telemetry", "ab")
-        dbfile_machines = open("./pickle_files/loading/machines", "ab")
-        dbfile_failures = open("./pickle_files/loading/failures", "ab")
-        dbfile_errors = open("./pickle_files/loading/errors", "ab")
-        dbfile_maint = open("./pickle_files/loading/maint", "ab")
+        with open("./pickle_files/loading/telemetry", "ab") as dbfile_telemetry:
+            pickle.dump(df_telemetry, dbfile_telemetry)
+        with open("./pickle_files/loading/machines", "ab") as dbfile_machines:
+            pickle.dump(df_machines, dbfile_machines)
+        with open("./pickle_files/loading/failures", "ab") as dbfile_failures:
+            pickle.dump(df_failures, dbfile_failures)
+        with open("./pickle_files/loading/errors", "ab") as dbfile_errors:
+            pickle.dump(df_errors, dbfile_errors)
+        with open("./pickle_files/loading/maint", "ab") as dbfile_maint:
+            pickle.dump(df_maintenance, dbfile_maint)
 
-        pickle.dump(df_telemetry, dbfile_telemetry)
-        pickle.dump(df_machines, dbfile_machines)
-        pickle.dump(df_failures, dbfile_failures)
-        pickle.dump(df_errors, dbfile_errors)
-        pickle.dump(df_maintenance, dbfile_maint)
-
-        dbfile_telemetry.close()
-        dbfile_machines.close()
-        dbfile_failures.close()
-        dbfile_errors.close()
-        dbfile_maint.close()
         return (
             df_telemetry,
             df_machines,
@@ -44,23 +46,22 @@ class Loading_files:
     def load_db_file(
         self,
     ) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
-        dbfile_telemetry = open("./pickle_files/loading/telemetry", "rb")
-        dbfile_machines = open("./pickle_files/loading/machines", "rb")
-        dbfile_failures = open("./pickle_files/loading/failures", "rb")
-        dbfile_errors = open("./pickle_files/loading/errors", "rb")
-        dbfile_maint = open("./pickle_files/loading/maint", "rb")
+        """
+        Loading pickle files.
 
-        df_telemetry = pickle.load(dbfile_telemetry)
-        df_machines = pickle.load(dbfile_machines)
-        df_failures = pickle.load(dbfile_failures)
-        df_errors = pickle.load(dbfile_errors)
-        df_maintenance = pickle.load(dbfile_maint)
+        Return: 5 dataframes for each required files
+        """
+        with open("./pickle_files/loading/telemetry", "rb") as dbfile_telemetry:
+            df_telemetry = pickle.load(dbfile_telemetry)
+        with open("./pickle_files/loading/machines", "rb") as dbfile_machines:
+            df_machines = pickle.load(dbfile_machines)
+        with open("./pickle_files/loading/failures", "rb") as dbfile_failures:
+            df_failures = pickle.load(dbfile_failures)
+        with open("./pickle_files/loading/errors", "rb") as dbfile_errors:
+            df_errors = pickle.load(dbfile_errors)
+        with open("./pickle_files/loading/maint", "rb") as dbfile_maint:
+            df_maintenance = pickle.load(dbfile_maint)
 
-        dbfile_telemetry.close()
-        dbfile_machines.close()
-        dbfile_failures.close()
-        dbfile_errors.close()
-        dbfile_maint.close()
         return (
             df_telemetry,
             df_machines,
